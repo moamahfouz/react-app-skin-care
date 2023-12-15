7b57f17fefcd6f696c6858ab3760b85ec4e7dea3
mohamed <moa.mahfouz@gmail.com>
2023-12-15 23:48:46 +0200

    home page content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,94 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-        </footer>
+        <div className="container-fluid">
+            <footer className="pt-5 mt-5 border-top text-right" dir="ltr">
+                <div className="w-100 p-3">
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">who we are?</summary>
+                        <p className="pl-3"> Our proprietary algorithm analyses your skin, lifestyle and environment to
+                            decode your
+                            skin
+                            needs. It then maps the right clean, active ingredients to design a personalised routine for
+                            you.</p>
+                    </details>
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">Privacy Policy</summary>
+                        <p className="pl-3"> We want you to know that your privacy is important to us, and we want to
+                            make you feel
+                            as
+                            comfortable as possible when using this Site. We will only process your personal data as
+                            your
+                            (gender, age,
+                            email, mobile number) following this Privacy Policy and relevant personal data regulation.
+                            This
+                            processing
+                            is a prerequisite for us being able to offer the Services to you.</p></details>
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">Cookies</summary>
+                        <p className="pl-3">Our website uses cookies to hold a user’s unique session identifiers,
+                            allowing us to
+                            serve
+                            correct information back to the user.“Session” cookies are temporary bits of information
+                            that are
+                            erased
+                            once you exit your Web browser window or otherwise turn your computer off. Session cookies
+                            are used
+                            to
+                            improve navigation on Web sites and to collect aggregate statistical information. This Site
+                            may use
+                            session
+                            cookies.</p></details>
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">Other Tracking Technologies</summary>
+                        <p className="pl-3">We may also use other tracking technologies like pixel tags, web beacons to
+                            enhance the
+                            user
+                            experience. But these tracking technologies only collect limited data such as cookie number,
+                            time
+                            and date
+                            of page viewing and the number of users visiting any particular page. These tags can not be
+                            rejected
+                            or
+                            removed manually.</p></details>
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">Disclaimer</summary>
+                        <p className="pl-3">The information contained on this website is for general information
+                            purposes only. The
+                            information is provided by [skin plans] and while we endeavour to keep the information up to
+                            date
+                            and
+                            correct, we make no representations or warranties of any kind, express or implied, about the
+                            completeness,
+                            accuracy, reliability, suitability, or availability concerning the website or the
+                            information,
+                            products,
+                            services, or related graphics contained on the website for any purpose. Any reliance you
+                            place on
+                            such
+                            information is therefore strictly at your own risk.</p></details>
+                    <details className="mb-3">
+                        <summary className="h5 mb-2">Terms Of Services</summary>
+                        <p className="pl-3">Your use of this Site shall be deemed to constitute your voluntary and
+                            express consent
+                            to be
+                            bound by the terms and conditions of these Terms of Use without limitation and/or
+                            qualification and
+                            we shall
+                            be entitled to enforce these Terms of Use in the same way as if you had signed these Terms
+                            of
+                            Use.</p>
+                    </details>
+                    <p>The information is provided by [skin plans] and while we endeavour to keep the information up to
+                        date and
+                        correct, we make no representations or warranties of any kind, express or implied, about the
+                        completeness,
+                        accuracy, reliability, suitability, or availability concerning the website or the information,
+                        products,
+                        services, or related graphics contained on the website for any purpose.</p></div>
+            </footer>
+
+        </div>
     );
 }
 
